69e18d161e1ed16e22b886b25c1c50ba3f056640
Absya 2:34:734 <absyadamitsasyiham@gmail.com>
2025-12-25 05:20:22 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+let musicStarted = false;
+let currentPage = 1;
+
+function showPage(n){
+  document.querySelectorAll(".page").forEach(p=>p.classList.remove("show"));
+  document.getElementById("page"+n).classList.add("show");
+  currentPage = n;
+}
+
+function nextPage(n){
+  showPage(n);
+
+  if(n === 2 && !musicStarted){
+    const bgm = document.getElementById("bgm");
+    bgm.volume = 0;
+    bgm.play();
+    fadeInMusic();
+    musicStarted = true;
+  }
+}
+
+function prevPage(n){
+  showPage(n);
+}
+
+function fadeInMusic(){
+  const audio = document.getElementById("bgm");
+  let v = 0;
+  let fade = setInterval(()=>{
+    if(v < 0.7){
+      v += 0.02;
+      audio.volume = v;
+    } else clearInterval(fade);
+  }, 120);
+}
+
+function toggleMusic(){
+  const a = document.getElementById("bgm");
+  a.paused ? a.play() : a.pause();
+}
+
